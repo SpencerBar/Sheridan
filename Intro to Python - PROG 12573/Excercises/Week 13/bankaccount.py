0c30fbf6dc15f358a87bba4c681c0a4379b6921a
SpencerBar <srdb45@gmail.com>
2023-12-07 16:19:41 -0500

    bank exercise

--- Intro to Python - PROG 12573/Excercises/Week 13/bankaccount.py
+++ Intro to Python - PROG 12573/Excercises/Week 13/bankaccount.py
@@ -1,12 +1,13 @@
 from datetime import date
 class Account:
     _accountID_ = 0
-    def __init__(self, balance = 0, annualInterest = .01):
+    _annualInterestRate_ = 0
+    def __init__(self, balance = 0):
         self.__ID = Account._accountID_
         Account._accountID_ +=1
         self.__balance = balance
         self.__dateMade = date
-        self.__anInR = annualInterest
+        self.__anInR = Account._annualInterestRate_
     def getID(self):
         return self.__ID
     def getBalance(self):
@@ -16,16 +17,42 @@ class Account:
     def getDateCreated(self):
         return self.__dateMade
     
-    def setBalance(self, balance):
-        self.__balance = balance
+    def setBalance(self):
+        balance = input("Enter a balance")
+        try:
+            if balance >= 0:
+                self.__balance = balance
+            else:    
+                print("Balance cannot be negative")
+                self.setBalance()
+        except Exception as err:
+            print(err)
+            self.setBalance()
+            
     def setAnnualInterest(self, interest):
         self.__anInR = interest
-    def getMonthlyIntRate(self):
+    def __getMonthlyIntRate(self):
         return
     def getMonthlyInt(self):
-        return
-    def withdraw(self,amount):
-        self.__balance -= amount
+        monthlyInt = self.__balance * self.__getMonthlyIntRate()
+    def withdraw(self):
+        amount = input('Enter an amount: ')
+        try:
+            if amount <= self.__balance:
+                self.__balance -= amount
+            else:
+                self.withdraw()
+        except Exception as err:
+            print(err)
+            self.withdraw()
         
-    def deposit(self, amount):
-        self.__balance += amount
+    def deposit(self):
+        amount = input('Enter an amount: ')
+        try:
+            if amount >=0:
+                self.__balance += amount
+            else:
+                self.deposit()
+        except Exception as err:
+            print(err)
+            self.deposit()
