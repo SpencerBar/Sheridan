648ca14343402359e948d8ec3195dcfd54c0aaed
SpencerBar <srdb45@gmail.com>
2024-01-10 20:39:34 -0500

    java oop W1L2

---
+++ Semester 2/OOP in Java/Week 01/ZOP-OOP1-Java-Week-01/src/yourturn3/MyRectangle.java
@@ -0,0 +1,12 @@
+package yourturn3;
+
+public class MyRectangle{
+    public static void main(String[] args){
+        int length = 5;
+        int width = 6;
+
+        int areaOfRectangle = length * width;
+        System.out.println(areaOfRectangle);
+        
+    }
+}
