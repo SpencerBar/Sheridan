7c9f5f48a5e62a3c2181720985c1739abb8e6233
SpencerBar <srdb45@gmail.com>
2024-01-15 18:00:45 -0500

    java week 1 lesson 2

--- Semester 2/OOP in Java/Week 01/ZOP-OOP1-Java-Week-01/src/yourturn3/MyRectangle.java
+++ Semester 2/OOP in Java/Week 01/ZOP-OOP1-Java-Week-01/src/yourturn3/MyRectangle.java
@@ -4,9 +4,7 @@ public class MyRectangle{
     public static void main(String[] args){
         int length = 5;
         int width = 6;
-
         int areaOfRectangle = length * width;
-        System.out.println(areaOfRectangle);
-        
+        System.out.println(areaOfRectangle); 
     }
 }
