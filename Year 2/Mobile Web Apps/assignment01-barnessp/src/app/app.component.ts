aab7850f20ea28c0a20d892c7785a4d7338adb0b
SpencerBar <srdb45@gmail.com>
2024-09-17 14:29:24 -0400

    webapps lab03 and assignment1 start

---
+++ Year 2/Mobile Web Apps/assignment01-barnessp/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+//Constant FACT for inch to cm conversion
+const FACT = 2.54;
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  //properties
+  title = 'assignment01-barnessp';
+  inch = "1";
+  cm = 2.54;
+}
+
+function toDecimal(fractionString = "") {
+  let decimal = 0; // default return value
+  let token = fractionString.split(/\s+/);
+
+  // if it is length 1 then it is either a number or a fraction, must try to split over "/" to split fractions
+  if(token.length == 1){
+      let parts = fractionString.split("/");
+
+      // if now length 1, is number. no conversion needed
+      if (parts.length == 1){
+        decimal = parseFloat(parts[0]);
+      }
+      else{
+        let numerator = parseFloat(parts[0]);
+        let denominator = parseFloat(parts[1]);
+        decimal = numerator / denominator;
+      }
+  if(token.length == 2){
+    let part1 = token[0];
+    let part2 = token[1];
+
+    }
+
+
+  }
+  return decimal;
+
+}
