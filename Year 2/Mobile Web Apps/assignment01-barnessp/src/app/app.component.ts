adc11bb19fce022dbc30f2444f50dc201834acd3
SpencerBar <srdb45@gmail.com>
2024-10-03 09:37:33 -0400

    week 3-5

--- Year 2/Mobile Web Apps/assignment01-barnessp/src/app/app.component.ts
+++ Year 2/Mobile Web Apps/assignment01-barnessp/src/app/app.component.ts
@@ -36,14 +36,34 @@ function toDecimal(fractionString = "") {
         let denominator = parseFloat(parts[1]);
         decimal = numerator / denominator;
       }
-  if(token.length == 2){
-    let part1 = token[0];
-    let part2 = token[1];
+  // if(token.length == 2){
+  //   let part1 = token[0];
+  //   let part2 = token[1];
+  //   let parts = fractionString.split("/");
+  //   let numerator = parseFloat(parts[0]);
+  //   let denominator = parseFloat(parts[1]);
+  //   decimal = parseFloat(part1) + numerator / denominator;
 
-    }
-
+    // }
 
   }
   return decimal;
 
 }
+
+// function convertInchToCm(){
+// //1. convert fraction inch to decimal inch
+//   let inch = toDecimal(this.inch);
+//  //2. unit conversion inch to cm
+//   let cm = inch *2.54;
+// }
+//  function convertCmToInch(){
+//   if(this.fractionUsed){
+//     this.inch = toFraction(...);
+//   }
+//   else
+//   {
+//     this.inch = inch.toString()
+//   }
+// }
+
