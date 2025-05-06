9779ff2affbfd953d6d986bc8e85bf33b3462260
SpencerBar <srdb45@gmail.com>
2025-05-06 11:35:12 -0400

    new semster start

--- Year 2/Mobile Web Apps/finals-barnessp/src/app/app.component.spec.ts
+++ Year 2/Mobile Web Apps/finals-barnessp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'exampractice-barnessp' title`, () => {
+  it(`should have the 'finals-barnessp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('exampractice-barnessp');
+    expect(app.title).toEqual('finals-barnessp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, exampractice-barnessp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, finals-barnessp');
   });
 });
